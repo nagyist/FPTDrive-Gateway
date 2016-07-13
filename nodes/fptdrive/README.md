221c049638667ad072d61f047654bfc0054bdd94
Bach Pham <pxbachs@gmail.com>
2016-07-13 14:18:44 +0700

    FPTDrive Nodejs lib: -pDistance: calculate distance between a point to a line -geo_distance: calculate distance from geo coordinates

---
+++ nodes/fptdrive/README.md
@@ -0,0 +1,16 @@
+FPTDive Javascript library
+====================
+
+
+Install
+-------
+
+Run the following command in the root directory of your Node-RED install
+
+    npm install fptdrive
+
+
+Usage
+-----
+
+
---
+++ nodes/fptdrive/fptdrive.js
@@ -0,0 +1,48 @@
+//var exports = module.exports = {};
+
+exports.pDistance =  function(x, y, x1, y1, x2, y2) {
+
+  var A = x - x1;
+  var B = y - y1;
+  var C = x2 - x1;
+  var D = y2 - y1;
+
+  var dot = A * C + B * D;
+  var len_sq = C * C + D * D;
+  var param = -1;
+  if (len_sq !== 0) //in case of 0 length line
+      param = dot / len_sq;
+
+  var xx, yy;
+
+  if (param < 0) {
+    xx = x1;
+    yy = y1;
+  }
+  else if (param > 1) {
+    xx = x2;
+    yy = y2;
+  }
+  else {
+    xx = x1 + param * C;
+    yy = y1 + param * D;
+  }
+
+  var dx = x - xx;
+  var dy = y - yy;
+  return [xx, yy, Math.sqrt(dx * dx + dy * dy)];
+};
+
+exports.geo_distance = function(lat1, lon1, lat2, lon2, unit) {
+	var radlat1 = Math.PI * lat1/180;
+	var radlat2 = Math.PI * lat2/180;
+	var theta = lon1-lon2;
+	var radtheta = Math.PI * theta/180;
+	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+	dist = Math.acos(dist);
+	dist = dist * 180/Math.PI;
+	dist = dist * 60 * 1.1515;
+	if (unit=="K") { dist = dist * 1.609344; }
+	if (unit=="N") { dist = dist * 0.8684; }
+	return dist;
+};
